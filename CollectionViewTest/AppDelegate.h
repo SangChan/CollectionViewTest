d35950b31f4f4676b06db706f544c6a921e1d66f
SangChan Lee <sangchan82@gmail.com>
2015-05-11 13:35:59 +0900

    Initial Commit

---
+++ CollectionViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewTest
+//
+//  Created by SangChan on 2015. 5. 11..
+//  Copyright (c) 2015년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
